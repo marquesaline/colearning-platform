a3cf0ab15bfc8d3f1201a824e43219f9c422b533
marquesaline <alinem_oliveira@yahoo.com>
2022-05-23 11:03:44 -0300

    feat: add uma palavra como logo no header

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,7 +7,7 @@ router.get('/', controller.index)
 router.get('/contato', controller.contato)
 router.get('/cadastro', controller.cadastro)
 router.get('/login', controller.login)
-router.get('/planos', controller.login)
+router.get('/planos', controller.planos)
 
 
 module.exports = router;
