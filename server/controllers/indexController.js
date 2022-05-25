3d81595eef4211bc9aab518d2e6a3fcfffbce1cd
marquesaline <alinem_oliveira@yahoo.com>
2022-05-25 15:36:45 -0300

    feat: add title por página

--- server/controllers/indexController.js
+++ server/controllers/indexController.js
@@ -1,9 +1,9 @@
 const controller = {
-    index: (req, res) => res.render('index'),
-    contato: (req, res) => res.render('contato'),
-    cadastro: (req, res) => res.render('cadastro'),
-    login: (req, res) => res.render('login'),
-    planos: (req, res) => res.render('planos')
+    index: (req, res) => res.render('index', { title: 'CoLearning' }),
+    contato: (req, res) => res.render('contato', { title: 'CoLearning - Contato' }),
+    cadastro: (req, res) => res.render('cadastro', { title: 'CoLearning - Cadastro' }),
+    login: (req, res) => res.render('login', { title: 'CoLearning - Login' }),
+    planos: (req, res) => res.render('planos', { title: 'CoLearning - Planos' })
 }
 
 module.exports = controller
