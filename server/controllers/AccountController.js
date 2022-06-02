8ed984c724bf1b777f4fec3800ef32f17082f9be
marquesaline <alinem_oliveira@yahoo.com>
2022-06-02 12:02:22 -0300

    feat: add tela de assinatura e ajustando o css

--- server/controllers/AccountController.js
+++ server/controllers/AccountController.js
@@ -4,7 +4,7 @@ const controller = {
     calendar: (req, res) => res.render('calendario', { title: 'Calendário' }),
     create: (req, res) => res.render('criar-agenda', { title: 'Criar Agenda' }),
     account: (req, res) => res.render('minha-conta', { title: 'Minha Conta'}),
-    signature: (req, res) => res.render('assinatura')
+    signature: (req, res) => res.render('assinatura', { title: 'Assinatura'})
     
     
 }
