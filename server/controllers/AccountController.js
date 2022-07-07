1528c3d68964c07386dc74e12d1484b46470c053
marquesaline <alinem_oliveira@yahoo.com>
2022-07-06 21:19:59 -0300

    feat: crud das agendas

--- server/controllers/AccountController.js
+++ server/controllers/AccountController.js
@@ -5,17 +5,17 @@ const path = require("path");
 const helper = {};
 
 helper.read = (fileName) =>
-  fs.readFileSync(path.join(__dirname, `../data/${fileName}`), "utf-8");
+    fs.readFileSync(path.join(__dirname, `../data/${fileName}`), "utf-8");
 
 helper.write = (fileName, data) =>
-fs.writeFileSync(
-    path.join(__dirname, `../data/${fileName}`),
-    JSON.stringify(data),
-    "utf-8"
-);
-
+    fs.writeFileSync(
+        path.join(__dirname, `../data/${fileName}`),
+        JSON.stringify(data),
+        "utf-8"
+    );
+//Constates de uso do JSON e de criação dos dados para a agenda
 const getAgendas = () => JSON.parse(helper.read("agenda.json"))
-const getAgendaPorId = (id) =>
+const getAgendaId = (id) =>
     getAgendas().find((agenda) => agenda.id == id)
 
 const setAgendas = (agendas) => helper.write("agenda.json", agendas);
@@ -29,60 +29,28 @@ const getNextId = async () => {
     const newId = parseInt(agenda[agenda.length - 1].id) + 1
     return newId
 }
-    
-// Controllers
+const getBusinessHours = async (daysOfWeek, startTime, endTime) => {
+    let businessHours = []
+    for (i = 0; i <= 6; i++) {
+        if (daysOfWeek[i] == null) {
 
-// acesso ao json
-controller.events = async (req, res) => res.json(await getEvents())
-controller.eventsId = async (req, res) =>
-    res.json(await getEventsId(req.params.id))
-
-//acessar as agendas
-controller.calendar = (req, res) => res.render('calendario', {
-    title: 'Calendário'
-}),
-
-controller.add = (req, res) => res.render('criar-agenda', { title: 'Criar Agenda' }),
-
-controller.create = async (req, res) => {
-    const agendas = await getAgendas()
-    const id = await getNextId()
-    const { title, duration, start, end, startTime, endTime } = req.body;
-    const newAgenda = {
-        id,
-        title,
-        duration,
-        start,
-        end,
-        startTime,
-        endTime,
-    };
-    agendas.push(newAgenda)
-    setAgendas(agendas)
-    res.redirect("/sucesso")
-}
-controller.editAgenda = async (req, res) => {
-    let agendas = await getAgendas()
-    agendas = agendas.map((agenda) => {
-        if(agenda.id == req.params.id) {
-            const { title, duration, start, end, startTime, endTime } = req.body;
-            return {
-                id: agenda.id,
-                title, 
-                duration, 
-                start,
-                end,
-                startTime,
-                endTime
-            }
         } else {
-            return agenda
+            businessHours.push({
+                daysOfWeek: daysOfWeek[i],
+                startTime: startTime[i],
+                endTime: endTime[i]
+            })
         }
-    })
-    setAgendas(agendas)
-    res.redirect('/sucesso')
+
+    }
+    return businessHours
 }
 
+//Tela principal com o calendário do usuário
+controller.calendar = async (req, res) => res.render('calendario', {
+    title: 'Calendário',
+    agendas: await getAgendas()
+})
 controller.account = (req, res) => res.render('minha-conta', { title: 'Minha Conta'}),
 controller.editAccount = (req, res) => {
     res.render('minha-conta-editar', {
@@ -95,8 +63,94 @@ controller.editSignature = (req, res) => {
         title: 'Editar - Assinatura'
     })
 }
-    
-    
+//Controllers Agenda 
+controller.addAgenda = async (req, res) => res.render('criar-agenda', {
+    title: 'Criar Agenda',
+    agendas: await getAgendas()
+}),
+controller.editAgenda = async (req, res) => {
+    const agenda = getAgendaId(req.params.id)
+    res.render('editar-agenda', {
+        agendas: await getAgendas(),
+        title: `Editar ${agenda.title}`,
+        agenda
+    })
+}
+controller.removeAgenda = async (req, res) => {
+    const agenda = getAgendaId(req.params.id)
+    res.render('excluir-agenda', {
+        agendas: await getAgendas(),
+        title: `Excluir Agenda ${agenda.title}`,
+        agenda
+    })
+}
+controller.events = async (req, res) => res.json(await getEvents())
+controller.eventsId = async (req, res) =>
+    res.json(await getEventsId(req.params.id))
 
+
+//Controllers de manipulação do JSON
+controller.createAgenda = async (req, res) => {
+    const agendas = await getAgendas()
+    const id = await getNextId()
+    const {
+        title,
+        duration,
+        start,
+        end,
+        daysOfWeek,
+        startTime,
+        endTime
+    } = req.body;
+    const businessHours = await getBusinessHours(daysOfWeek, startTime, endTime)
+    const newAgenda = {
+        id,
+        title,
+        duration,
+        start,
+        end,
+        businessHours
+    };
+    agendas.push(newAgenda)
+    setAgendas(agendas)
+    res.redirect("/sucesso")
+}
+controller.updateAgenda = async (req, res) => {
+    let agendas = await getAgendas()
+    agendas = agendas.map((agenda) => {
+        if (agenda.id == req.params.id) {
+            const {
+                title,
+                duration,
+                start,
+                end,
+                daysOfWeek,
+                startTime,
+                endTime
+            } = req.body;
+            const businessHours = getBusinessHours(daysOfWeek, startTime, endTime)
+            return {
+                id: agenda.id,
+                title,
+                duration,
+                start,
+                end,
+                startTime,
+                endTime
+            }
+        } else {
+            return agenda
+        }
+    })
+    setAgendas(agendas)
+    res.redirect('/sucesso')
+}
+controller.deleteAgenda = (req, res) => {
+    const agenda = getAgendaId(req.params.id)
+    res.render("agenda-excluir", {
+        title: `Excluir Usuário ${agenda.title}`,
+        agenda,
+    });
+}
 
 module.exports = controller
