a81aa6d8b8030c1aa64d670c383a11aa3c619063
marquesaline <alinem_oliveira@yahoo.com>
2022-07-07 21:10:30 -0300

    fix: conflitos

--- server/app.js
+++ server/app.js
@@ -2,7 +2,8 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-const methodOverride = require('method-override')                 
+var logger = require('morgan');
+const methodOverride = require('method-override')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -12,15 +13,12 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
-
-app.use(express.json());
+app.use(logger('dev'));
 app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride('_method'))
-
-
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
